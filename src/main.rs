4075b90de9864d3407cb02387497588a698ca357
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-10-04 23:34:29 +0200

    refactor(main): Use database wrapper for fetching user

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,26 @@ extern crate serde;
 #[macro_use]
 extern crate serde_json;
 
+mod database;
+
 #[get("/users/sorin", format = "application/activity+json")]
 fn user() -> rocket_contrib::Json<serde_json::Value> {
+    let user = database::users::fetch();
+
     rocket_contrib::Json(json!({
         "@context": [
             "https://www.w3.org/ns/activitystreams",
         ],
-        "id": "http://localhost:8000/users/sorin",
+        "id": format!("http://localhost:8000/users/{}", user.username),
         "type": "Person",
-        "following": "http://localhost:8000/users/sorin/following",
-        "followers": "http://localhost:8000/users/sorin/followers",
-        "inbox": "http://localhost:8000/users/sorin/inbox",
-        "outbox": "http://localhost:8000/users/sorin/outbox",
-        "preferredUsername": "sorin",
-        "name": "Sorin Davidoi",
-        "summary": "Cobalt user.",
-        "url": "http://localhost:8000/@sorin",
+        "following": format!("http://localhost:8000/users/{}/following", user.username),
+        "followers": format!("http://localhost:8000/users/{}/followers", user.username),
+        "inbox": format!("http://localhost:8000/users/{}/inbox", user.username),
+        "outbox": format!("http://localhost:8000/users/{}/outbox", user.username),
+        "preferredUsername": user.username,
+        "name": user.name,
+        "summary": user.summary,
+        "url": format!("http://localhost:8000/@{}", user.username),
     }))
 }
 
