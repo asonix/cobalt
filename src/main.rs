aa7a6138bf56da188a53691cde7f17282b5821d7
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-10-04 23:02:02 +0200

    feat(main): Add user endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+extern crate rocket_contrib;
+extern crate serde;
+#[macro_use]
+extern crate serde_json;
+
+#[get("/users/sorin", format = "application/activity+json")]
+fn user() -> rocket_contrib::Json<serde_json::Value> {
+    rocket_contrib::Json(json!({
+        "@context": [
+            "https://www.w3.org/ns/activitystreams",
+        ],
+        "id": "http://localhost:8000/users/sorin",
+        "type": "Person",
+        "following": "http://localhost:8000/users/sorin/following",
+        "followers": "http://localhost:8000/users/sorin/followers",
+        "inbox": "http://localhost:8000/users/sorin/inbox",
+        "outbox": "http://localhost:8000/users/sorin/outbox",
+        "preferredUsername": "sorin",
+        "name": "Sorin Davidoi",
+        "summary": "Cobalt user.",
+        "url": "http://localhost:8000/@sorin",
+    }))
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![user]).launch();
 }
