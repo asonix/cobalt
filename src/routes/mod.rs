1620778b0bbbe6b3ed853f84bf4510f0d11b1d7b
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-10-05 23:53:30 +0200

    feat(routes): Implement FromRequest trait for Connection

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -29,3 +29,18 @@ fn user_followers(username: String) -> rocket_contrib::Json<serde_json::Value> {
 fn user_outbox(username: String) -> rocket_contrib::Json<serde_json::Value> {
     rocket_contrib::Json(database::users::fetch_outbox(username).as_activity_pub())
 }
+
+impl<'a, 'r> rocket::request::FromRequest<'a, 'r> for database::Connection {
+    type Error = ();
+
+    fn from_request(
+        request: &'a rocket::Request<'r>,
+    ) -> rocket::request::Outcome<database::Connection, ()> {
+        let pool = request.guard::<rocket::State<database::Pool>>()?;
+
+        match pool.get() {
+            Ok(connection) => rocket::Outcome::Success(database::Connection(connection)),
+            Err(_) => rocket::Outcome::Failure((rocket::http::Status::ServiceUnavailable, ())),
+        }
+    }
+}
