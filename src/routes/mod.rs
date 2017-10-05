2748f6d0019166d52a07a70d46ebb5e8b5637323
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-10-05 16:31:40 +0200

    feat(routes): Take username parameter

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -10,7 +10,7 @@ pub fn create() -> Vec<rocket::Route> {
     routes!(user)
 }
 
-#[get("/users/sorin", format = "application/activity+json")]
-fn user() -> rocket_contrib::Json<serde_json::Value> {
+#[get("/users/<_username>", format = "application/activity+json")]
+fn user(_username: String) -> rocket_contrib::Json<serde_json::Value> {
     rocket_contrib::Json(database::users::fetch().as_activity_pub())
 }
