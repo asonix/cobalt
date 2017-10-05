9880f04a4f07aa74a935f09c90edd0af26bd467c
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-10-05 17:53:31 +0200

    feat(routes): Add followers route

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -7,10 +7,15 @@ use activity_pub::ActivityPub;
 use database;
 
 pub fn create() -> Vec<rocket::Route> {
-    routes!(user)
+    routes!(user, user_followers)
 }
 
 #[get("/users/<username>", format = "application/activity+json")]
 fn user(username: String) -> rocket_contrib::Json<serde_json::Value> {
     rocket_contrib::Json(database::users::fetch(username).as_activity_pub())
+}
+
+#[get("/users/<username>/followers", format = "application/activity+json")]
+fn user_followers(username: String) -> rocket_contrib::Json<serde_json::Value> {
+    rocket_contrib::Json(database::users::fetch_followers(username).as_activity_pub())
 }
