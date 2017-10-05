622d3140845c48796533c8e71b2edef7c6c6f0d9
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-10-05 15:58:39 +0200

    feat(activity_pub): Implement ActivityPub trait for User

--- src/activity_pub/mod.rs
+++ src/activity_pub/mod.rs
@@ -1,7 +1,29 @@
 extern crate serde_json;
 
+use database;
+
 pub trait ActivityPub {
     fn as_activity_pub(&self) -> serde_json::Value {
         panic!("Not implemented!");
+    }
+}
+
+impl ActivityPub for database::users::User {
+    fn as_activity_pub(&self) -> serde_json::Value {
+        json!({
+            "@context": [
+                "https://www.w3.org/ns/activitystreams",
+            ],
+            "id": format!("http://localhost:8000/users/{}", self.username),
+            "type": "Person",
+            "following": format!("http://localhost:8000/users/{}/following", self.username),
+            "followers": format!("http://localhost:8000/users/{}/followers", self.username),
+            "inbox": format!("http://localhost:8000/users/{}/inbox", self.username),
+            "outbox": format!("http://localhost:8000/users/{}/outbox", self.username),
+            "preferredUsername": self.username,
+            "name": self.name,
+            "summary": self.summary,
+            "url": format!("http://localhost:8000/@{}", self.username),
+        })
     }
 }
