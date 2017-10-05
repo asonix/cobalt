87cb9e28cd83488b5255982806f1c9c9089e07e7
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-10-05 17:51:17 +0200

    feat(database): Fetch followers

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -5,11 +5,29 @@ pub mod users {
         pub summary: String,
     }
 
+    pub struct Followers {
+        pub username: String,
+        pub followers: Vec<User>,
+    }
+
     pub fn fetch(_username: String) -> User {
         User {
             username: "sorin".to_string(),
             name: "Sorin Davidoi".to_string(),
             summary: "Cobalt user".to_string(),
+        }
+    }
+
+    pub fn fetch_followers(username: String) -> Followers {
+        Followers {
+            username,
+            followers: vec![
+                User {
+                    username: "ghost".to_string(),
+                    name: "Ghost".to_string(),
+                    summary: "Cobalt ghost user.".to_string(),
+                },
+            ],
         }
     }
 }
