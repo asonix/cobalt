cd7a2a1b91fb881c3b0b3d1547ab6efe6653e01c
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-10-05 23:48:54 +0200

    feat(database): Connection pool

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,3 +1,32 @@
+extern crate diesel;
+extern crate r2d2;
+extern crate r2d2_diesel;
+extern crate std;
+
+use self::r2d2_diesel::ConnectionManager;
+
+pub const DATABASE_FILE: &'static str = env!("DATABASE_URL");
+
+pub type Pool = r2d2::Pool<ConnectionManager<diesel::PgConnection>>;
+
+pub struct Connection(pub r2d2::PooledConnection<ConnectionManager<diesel::PgConnection>>);
+
+impl std::ops::Deref for Connection {
+    type Target = diesel::PgConnection;
+
+    #[inline(always)]
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub fn create_connection_pool() -> Pool {
+    let config = r2d2::Config::default();
+    let manager = ConnectionManager::<diesel::PgConnection>::new(DATABASE_FILE);
+
+    r2d2::Pool::new(config, manager).unwrap()
+}
+
 pub mod users {
     pub struct User {
         pub username: String,
