0b892010767d3c3f6e2c9e55296a9eed9b5819a5
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-10-10 21:16:10 +0200

    feat(migrations/create_users): Create remote_users table

--- migrations/2017-10-05-210546_create_users/up.sql
+++ migrations/2017-10-05-210546_create_users/up.sql
@@ -6,4 +6,16 @@ CREATE TABLE users (
     summary TEXT NOT NULL
 );
 
+CREATE TABLE remote_users (
+    id VARCHAR PRIMARY KEY,
+    url VARCHAR UNIQUE NOT NULL,
+    inbox VARCHAR UNIQUE NOT NULL,
+    outbox VARCHAR UNIQUE NOT NULL,
+    following VARCHAR UNIQUE NOT NULL,
+    followers VARCHAR UNIQUE NOT NULL,
+    username VARCHAR UNIQUE NOT NULL,
+    name VARCHAR NOT NULL,
+    summary TEXT NOT NULL
+);
+
 INSERT INTO users (id, password, username, name, summary) VALUES (0, 'admin', 'admin', 'Admin', 'The admin.');
